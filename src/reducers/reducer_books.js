84a845a53adbb1e02773af39a95bd3e9b8bac3a9
Worldofwbdesign <zavodzamutov@gmail.com>
2017-06-20 21:33:55 +0300

    add books reducer and book_list component

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'The Witcher' },
+    { title: 'Th Three Mushketters' },
+    { title: 'The Little Prince' }
+  ]
+}
